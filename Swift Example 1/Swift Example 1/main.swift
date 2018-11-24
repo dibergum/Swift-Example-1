10a00e6289a2ae53358947b6b3037c2bbbbc8e46
BuildTools <unconfigured@null.spigotmc.org>
2018-11-24 13:47:56 -0500

    Created several variables, Int, String and Double,  printed values and wrote a for loop.

---
+++ Swift Example 1/Swift Example 1/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by David Bergum on 11/24/18.
+//  Copyright © 2018 Red Sky Testing. All rights reserved.
+//
+
+import Foundation
+
+var name = "Dave"
+var age: Int = 76
+var value: Double = 123456
+
+name = "Josh"
+age = 24
+
+for i in 1...5 {
+    print("i = \(i)")
+}
